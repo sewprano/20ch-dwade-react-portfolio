65e459049f2e43ff13f258fe95c4abf2d253796c
Deborah Wade <sopranosamurai@yahoo.com>
2023-01-06 12:00:37 -0800

    simplified component structure by removing folders

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <h1>Deborah Wade</h1>
+            </div>
+        </header>
+    )
+}
