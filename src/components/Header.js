19fff8efc6746b8d661a27053231a7bce7c55d97
Deborah Wade <sopranosamurai@yahoo.com>
2023-01-07 10:18:59 -0800

    fixed errors to render page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
-import Footer from '../components/Navigation.js';
+import Navigation from '../components/Navigation.js';
 
 const Header = ({displayedPage, changePage}) => {
     return (
-        <header className={Header}>
+        <header className='Header'>
             <div>
                 <h1>Deborah Wade</h1>
             </div>
-            <Footer />
+            <Navigation displayedPage={displayedPage} changePage={changePage} />
         </header>
     )
 }
