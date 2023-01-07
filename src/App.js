4d1c6b4c045b2a9ac32feb31a64e1772776c4dd3
Deborah Wade <sopranosamurai@yahoo.com>
2023-01-06 20:52:45 -0800

    simplified by changing from router to useState

--- src/App.js
+++ src/App.js
@@ -1,34 +1,59 @@
 //import components:
 import Header from './components/Header';
-import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 
-//import router
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+//import pages:
+import Portfolio from './pages/Portfolio.js';
+import About from './pages/About.js';
+import Contact from './pages/Contact.js';
+import Resume from './pages/Resume.js';
+import { useState } from 'react';
+
  
-const App = () => {
+function App(){
+  const [displayedPage, setDisplayedPage] = useState('about');
+
+  const renderPage = () => {
+    if (displayedPage === 'about') return <About />;
+    if (displayedPage === 'portfolio') return <Portfolio />;
+    if (displayedPage === 'contact') return <Contact />;
+    if (displayedPage === 'resume') return <Resume />;
+  }
+
+  const changePage = (page) => setDisplayedPage(page);
+
   return (
     <>
-    <Router>
-      <Header />
-      <Navigation />
-      <Routes>
-        <Route path={'/'} 
-        element={<About />}
-        />
-        <Route path={'portfolio'}
-        element={<Portfolio />}
-        />
-        <Route path={'contact'}
-        element={<Contact />}
-        />
-        <Route path={'resume'}
-        element={<Resume />}
-        />
-      </Routes>
-      <Footer />
-    </Router>
+     <Header displayedPage={displayedPage} chamgePage={changePage}/>
+     {renderPage}
+     <Footer />
     </>
   )
 }
+
 export default App;
+
+
+// return (
+  //   <>
+  //   <Router>
+  //     <Header />
+  //     {/* <Navigation /> */}
+  //     <Routes>
+  //       <Route path={'/'} 
+  //       element={<About />}
+  //       />
+  //       <Route path={'portfolio'}
+  //       element={<Portfolio />}
+  //       />
+  //       <Route path={'contact'}
+  //       element={<Contact />}
+  //       />
+  //       <Route path={'resume'}
+  //       element={<Resume />}
+  //       />
+  //     </Routes>
+  //     <Footer />
+  //   </Router>
+  //   </>
+  // )
---
+++ src/components/Navigation.js
@@ -0,0 +1,23 @@
+const Navigation = ({displayPage, changePage}) => {
+    return (
+        <nav>
+            <button className={displayPage === 'about' ? StyleSheet.selected : StyleSheet.Button}
+            onClick={ () => changePage('about')}>
+                About Deborah
+            </button>
+            <button className={displayPage === 'portfolio' ? StyleSheet.selected : StyleSheet.Button}
+            onClick={ () => changePage('portfolio')}>
+                Portfolio
+            </button>
+            <button className={displayPage === 'contact' ? StyleSheet.selected : StyleSheet.Button}
+            onClick={ () => changePage('contact')}>
+                Contact
+            </button>
+            <button className={displayPage === 'resume' ? StyleSheet.selected : StyleSheet.Button}
+            onClick={ () => changePage('resume')}>
+                Resume
+            </button>
+        </nav>
+    )
+}
+export default Navigation;
